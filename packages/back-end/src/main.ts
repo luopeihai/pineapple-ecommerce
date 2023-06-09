c0dd57ad023222106147f2ade9aadd7296c88ca0
Peihai Luo （罗培海） <phluo@trip.com>
2023-06-09 17:41:54 +0800

    feat: limit require

--- packages/back-end/src/main.ts
+++ packages/back-end/src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
+import rateLimit from 'express-rate-limit';
 import { AppModule } from './app.module';
 import { getServerConfig } from "./config";
 import { ConfigEnum } from "./config/enum"
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -11,6 +13,13 @@ async function bootstrap() {
 
   const port = config[ConfigEnum.APP_PORT]
   app.setGlobalPrefix('api/v1');
+  // rateLimit限流
+  app.use(
+    rateLimit({
+      windowMs: 1 * 60 * 1000, // 1 minutes
+      max: 300, // limit each IP to 100 requests per windowMs
+    }),
+  );
 
   await app.listen(typeof port === 'string' ? parseInt(port) : 3000)
 
